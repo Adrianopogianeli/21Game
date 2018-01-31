3e9504b497edfc0e412f867340f4fecdd250727f
adrianopogianeli <adriano.pogianeli@gmail.com>
2018-01-31 21:44:24 -0200

    first

--- app/src/main/java/br/com/adriano/a21game/SplashActivity.kt
+++ app/src/main/java/br/com/adriano/a21game/SplashActivity.kt
@@ -1,18 +1,40 @@
 package br.com.adriano.a21game
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import br.com.adriano.a21game.R.layout.activity_splash
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(activity_splash)
+
         carregar()
     }
 
     fun carregar(){
-        
+
+        val animacao = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
+
+        ivLogo.clearAnimation()
+        ivLogo.startAnimation(animacao)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity,
+                    MainActivity::class.java)
+
+            startActivity(intent)
+
+            this@SplashActivity.finish()
+
+        },300)
     }
 
 }
